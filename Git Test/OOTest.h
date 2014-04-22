36302f60328cf61cb5c1a4eb74173bc7b573bcb9
breath2 <breath2@breath2mini.local>
2014-04-22 14:39:50 +0800

    added a test class

---
+++ Git Test/OOTest.h
@@ -0,0 +1,13 @@
+//
+//  OOTest.h
+//  Git Test
+//
+//  Created by breath2 on 14-4-22.
+//  Copyright (c) 2014年 breath2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OOTest : NSObject
+
+@end
---
+++ Git Test/OOTest.m
@@ -0,0 +1,13 @@
+//
+//  OOTest.m
+//  Git Test
+//
+//  Created by breath2 on 14-4-22.
+//  Copyright (c) 2014年 breath2. All rights reserved.
+//
+
+#import "OOTest.h"
+
+@implementation OOTest
+
+@end
